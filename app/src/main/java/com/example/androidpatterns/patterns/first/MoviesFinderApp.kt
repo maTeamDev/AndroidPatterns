c1e70bedfb6a7c8ab294f5d88bc89aee52fcac17
Andrey Oleynik <aoleynik@chegg.com>
2019-10-23 19:17:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidpatterns/patterns/first/MoviesFinderApp.kt
@@ -0,0 +1,43 @@
+package com.example.androidpatterns.patterns.first
+
+
+/**
+ * In this task we will developed MoviesFinderApp that will search for a movies from two different servers and print search results to console
+ *
+ * 1) You need to create two different classes (IMDB and Kinopoisk). Each of them should represent the movies server and contains a set of unique movies
+ * 2) The movie should have next params: id (random and unique), name, year, genre
+ * 3) Each server should contain next functions:
+ * - find movie by name in both servers
+ * - get all movies by specific genre from both servers
+ * - get all movies newest or equals to passed year
+ **/
+
+
+/**
+ * Use this class as a start point
+ */
+class MoviesFinderApp {
+
+    fun printMovieByName(name:String){
+        TODO("Implementation")
+    }
+
+
+}
+
+
+
+fun main() {
+    val moviesFinder = MoviesFinderApp()
+
+    moviesFinder.printMovieByName("Joker")
+
+
+
+}
+
+
+
+
+
+
