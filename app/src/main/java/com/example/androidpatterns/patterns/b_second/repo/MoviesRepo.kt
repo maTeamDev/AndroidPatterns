520b87758a3ba9c8e276b58e0692075e454147a8
Alexandra <sluckaya1992@gmail.com>
2019-11-12 20:19:49 +0200

    added movie factory class

--- app/src/main/java/com/example/androidpatterns/patterns/b_second/repo/MoviesRepo.kt
+++ app/src/main/java/com/example/androidpatterns/patterns/b_second/repo/MoviesRepo.kt
@@ -7,26 +7,40 @@ import com.example.androidpatterns.patterns.b_second.utils.NetworkManager
 enum class Genre{
     DRAMA,
     COMEDY,
-    TRILLER
+    THRILLER
 }
 
-fun createMovie(id: Int, name: String, year: Int, genre: Genre, serverName: String ): Movie{
-    return Movie(id,
-        name,
-        year,
-        genre,
-        serverName,
-        System.currentTimeMillis())
+class MovieFactory(){
+
+    fun createMovie(id: Int, name: String, year: Int, genre: Genre, serverName: String ): Movie{
+        return Movie(id,
+            name,
+            year,
+            genre,
+            serverName,
+            System.currentTimeMillis())
+    }
+
+    fun createMovieImdb(id: Int, name: String, year: Int, genre: Genre): Movie{
+        return createMovie(id, name, year, genre, "Imdb")
+    }
+
+    fun createMovieKinopoisk(id: Int, name: String, year: Int, genre: Genre): Movie{
+        return createMovie(id, name, year, genre, "Kinopoisk")
+    }
+
 }
+
 
 class IMDB {
     private val logger = Logger()
     private val networkManager = NetworkManager(logger)
+    private val movieFactory = MovieFactory()
     private val movies = mutableListOf(
-        createMovie(1, "Sherlock", 2000, Genre.DRAMA, "imdb"),
-        createMovie(2, "Ghost hunters", 2019, Genre.COMEDY, "imdb"),
-        createMovie(3, "Spanch Bob", 2019, Genre.DRAMA, "imdb"),
-        createMovie(4, "Wheel", 2005, Genre.TRILLER, "imdb")
+        movieFactory.createMovieImdb(1, "Sherlock", 2000, Genre.DRAMA),
+        movieFactory.createMovieImdb(2, "Ghost hunters", 2019, Genre.COMEDY),
+        movieFactory.createMovieImdb(3, "Spanch Bob", 2019, Genre.DRAMA),
+        movieFactory.createMovieImdb(4, "Wheel", 2005, Genre.THRILLER)
     )
 
     fun getMovieByName(name: String): List<Movie> {
@@ -51,11 +65,12 @@ class IMDB {
 class Kinopoisk {
     private val logger = Logger()
     private val networkManager = NetworkManager(logger)
+    private val movieFactory = MovieFactory()
     private val movies = mutableListOf(
-        createMovie(1, "Sherlock", 2000, Genre.DRAMA, "Kinopoisk"),
-        createMovie(2, "Ghost hunters", 2019, Genre.COMEDY, "Kinopoisk"),
-        createMovie(3, "Spanch Bob", 2019, Genre.DRAMA, "Kinopoisk"),
-        createMovie(4, "Wheel", 2005, Genre.TRILLER, "Kinopoisk")
+        movieFactory.createMovieImdb(1, "Sherlock", 2000, Genre.DRAMA),
+        movieFactory.createMovieImdb(2, "Ghost hunters", 2019, Genre.COMEDY),
+        movieFactory.createMovieImdb(3, "Spanch Bob", 2019, Genre.DRAMA),
+        movieFactory.createMovieImdb(4, "Wheel", 2005, Genre.THRILLER)
     )
 
     fun getMovieByName(name: String): List<Movie> {
